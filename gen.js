36bb395f0916d227981bf23b0a8c7364615969d1
docsploit <robertdevasia@outlook.com>
2023-03-16 12:16:01 +0530

    bumpd to 1.0.20 espress

--- gen.js
+++ gen.js
@@ -2,11 +2,12 @@ const chalk = require('chalk')
 const fs = require('fs')
 const path = require('path')
 const { format } = require('prettier')
-const { scan, capitalizeFirstLetter } = require('./utility')
+const { scan, capitalizeFirstLetter } = require('./utility');
+
 async function generate(version) {
     if (fs.existsSync(path.join(process.cwd(), '.espressive'))) {
         const esp = fs.readFileSync(path.join(process.cwd(), '.espressive')).toString('utf-8')
-        const espObj = JSON.parse(esp)
+        const espObj = JSON.parse(esp);
         if (version === espObj.framework.version) {
             let moduleName = await scan('Module Name :', 'Module name is required!');
             moduleName = moduleName.toLowerCase();
@@ -17,9 +18,7 @@ async function generate(version) {
             console.log(`CREATE ${chalk.cyan(controllerPath)}`);
             fs.writeFileSync(controllerPath, format(`
             import {Controller} from "@docsploit/espress";
-            import {GET,POST,PUT,DELETE} from "@docsploit/espress/lib/methods";
-            import {sendErrorResponse,sendSuccessResponse} from "@docsploit/espress/lib/utils";
-            @Controller
+            @Controller('/${moduleName.toLowerCase()}')
             export default class ${capitalizeFirstLetter(moduleName)}{}
             `, { parser: 'typescript' }))
             console.log(`CREATE ${chalk.cyan(modelPath)}`);
@@ -27,25 +26,11 @@ async function generate(version) {
                 export type ${capitalizeFirstLetter(moduleName)}Type = {}
                 // Any model related works should be done here.
             `, { parser: 'typescript' }))
-            console.log(`UPDATE ${chalk.cyan(path.join(process.cwd(), 'app', 'modules.ts'))}`);
-            updateModulesTs(moduleName)
         } else {
             console.log(chalk.yellow.bold`Framework version and cli version not match cli-version - ${version} frameworkVersion - ${espObj.framework.version}`)
         }
     } else {
         console.log(chalk.redBright.bold`Not an Espressive Project`)
     }
-}
-
-function updateModulesTs(moduleName) {
-    const file = fs.readFileSync(path.join(process.cwd(), 'app', 'modules.ts'), 'utf-8');
-    const lines = file.split('\n');
-    lines.splice(0, 0, `import ${capitalizeFirstLetter(moduleName)} from './${moduleName}/${moduleName}.controller'`)
-    lines.pop();
-    lines.pop();
-    const signModule = `register('/${moduleName}',${capitalizeFirstLetter(moduleName)});\n\nexport default this;`
-    const newFile = lines.join('\n')
-    fs.writeFileSync(path.join(process.cwd(), 'app', 'modules.ts'), format(newFile, { parser: 'typescript', }));
-    fs.appendFileSync(path.join(process.cwd(), 'app', 'modules.ts'), format(signModule, { parser: 'typescript' }))
 }
 module.exports = generate
--- init/init.js
+++ init/init.js
@@ -6,101 +6,171 @@ const figlet = require("figlet");
 const { scan, capitalizeFirstLetter, renderTemplate } = require("../utility");
 const { spawnSync } = require("child_process");
 const prettier = require("prettier");
+
 async function execute(version) {
+
   try {
+
     console.log(chalk.bold("Espressive - Create New Project"));
+
     const project = await scan(
+
       "Project Name : ",
+
       chalk.red("Project name is required")
+
     );
+
     const desc = await scan("Description : ", "Description is required");
+
     const author = await scan("Author : ", "Author name is required");
 
     const espressiveObject = {
+
       name: project,
+
       description: desc,
+
       author,
+
       framework: {
+
         version,
+
         assets: ["/static"],
+
       },
+
       module: {
+
         root: "app",
+
       },
+
     };
+
     await createDirectory(project, espressiveObject);
+
     installDependencies(project);
+
   } catch (error) {
+
     console.log(error);
+
   }
 }
 
 function createDirectory(name, espressiveObject) {
+
   return new Promise((resolve, reject) => {
+
     if (fs.existsSync(path.join(process.cwd(), name))) {
+
       throw Error("FolderExistException : Folder already exist");
+
     } else {
+
       console.log(chalk.blueBright("Creating espressive folder..."));
+
       fs.mkdirSync(path.join(process.cwd(), name));
+      
       fs.writeFileSync(
+
         path.join(process.cwd(), name, ".espressive"),
+
         prettier.format(JSON.stringify(espressiveObject), { parser: "json" })
+
       );
+
       fs.mkdirSync(path.join(process.cwd(), name, "app"));
+
+      
       fs.writeFileSync(
-        path.join(process.cwd(), name, "app", "modules.ts"),
-        prettier.format(
-          'import {register} from "@docsploit/espress";\n\n// register modules here\n\nexport default this',
-          { parser: "typescript" }
-        )
-      );
-      fs.writeFileSync(
+
         path.join(process.cwd(), name, "index.ts"),
+        
         prettier.format(
           `
                 import {Server} from '@docsploit/espress';
-                import modules from './app/modules'
-                const server = new Server({limit:'100mb'},modules,'${capitalizeFirstLetter(
-                  name
-                )}');
+
+
+                const server = new Server('${capitalizeFirstLetter(name)}');
+
+                //register controllers here eg: server.register(ExampleController)
                 // add code here
-                server.run();
+                server.run({});
             `,
+
           { parser: "typescript" }
+
         )
+
       );
+      
       const pkgJson = fs
+
         .readFileSync(
+
           path.join(__dirname, "src_template", "package.json.template")
+
         )
+
         .toString("utf-8");
+
       const renderPkgJson = renderTemplate(pkgJson, {
+
         name,
+
         desc: espressiveObject.description,
+
         version: espressiveObject.framework.version,
+
       });
+
       fs.writeFileSync(
+
         path.join(process.cwd(), name, "package.json"),
+
         renderPkgJson
+
       );
+
       const gitignore = fs
+      
         .readFileSync(
+
           path.join(__dirname, "src_template", ".gitignore.template")
+
         )
+
         .toString("utf-8");
+
       fs.writeFileSync(path.join(process.cwd(), name, ".gitignore"), gitignore);
+
       const tsConfig = fs
+
         .readFileSync(path.join(__dirname, "src_template", "tsconfig.template"))
+
         .toString("utf-8");
+
       fs.writeFileSync(
+
         path.join(process.cwd(), name, "tsconfig.json"),
+
         tsConfig
+
       );
+
       fs.mkdirSync(path.join(process.cwd(), name, ".vscode"));
+
       fs.mkdirSync(path.join(process.cwd(), name, "utils"));
+
       fs.mkdirSync(path.join(process.cwd(), name, "static"));
+
       fs.writeFileSync(
+
         path.join(process.cwd(), name, "static", "404.html"),
+
         `<!DOCTYPE html>
 <html lang="en">
 <head>
@@ -121,31 +191,56 @@ function createDirectory(name, espressiveObject) {
     <h1>404</h1>
 </body>
 </html>`
+
       );
+
       fs.writeFileSync(
+
         path.join(process.cwd(), name, ".vscode", "settings.json"),
+
         JSON.stringify({
+
           "files.exclude": {
+
             "**/.git": true,
+
             "**/.svn": true,
+
             "**/.hg": true,
+
             "**/CVS": true,
+
             "**/.DS_Store": true,
+
             "**/Thumbs.db": true,
+
             "**/node_modules": true,
+
           },
+
         })
+
       );
+
       resolve();
+
     }
+
   });
+
 }
 
 function installDependencies(name) {
+
   console.log(chalk.green`Installing Dependencies`);
+
   spawnSync("npm", ["install", "--prefix", path.join(process.cwd(), name)], {
+
     stdio: "inherit",
+
   });
+
 }
+
 
 module.exports = execute;
